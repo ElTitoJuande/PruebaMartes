7038867a8d0fcbd915b35fbc05a6fbce01f173fa
unknown <juandepm00@gmail.com>
2024-05-21 19:07:48 +0200

    jaimito

--- PruebaMartes.java
+++ PruebaMartes.java
@@ -1,6 +1,7 @@
 public class pruebaMartes{
     public static void main(Strings[] args) {
-        System.out.println("Pablito ")
-        System.out.println("Juande ")
+        System.out.println("Pablito");
+        System.out.println("Juande");
+        System.out.println("Jaimito");
     }
 }
